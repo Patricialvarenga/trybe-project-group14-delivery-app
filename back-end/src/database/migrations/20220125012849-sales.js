78bc94936c26f6ff477265e54391ec36f861f888
Patricia Silvestre Alvarenga <patriciasilvestrea@gmail.com>
2022-01-25 15:00:02 -0300

    fix: id migration sales

--- back-end/src/database/migrations/20220125012849-sales.js
+++ back-end/src/database/migrations/20220125012849-sales.js
@@ -31,6 +31,12 @@ module.exports = {
           key: 'id',
         },
       },
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
       userId: {
         type: Sequelize.INTEGER,
         allowNull: false,
