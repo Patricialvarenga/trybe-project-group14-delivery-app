ac715280e9190062bf4b00c931e75d48fa87d9f2
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2022-01-26 13:16:41 -0300

    rotas implementadas

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Login } from './pages';
+import {
+  Admin,
+  Checkout,
+  CustomerDetails,
+  CustomerOrders,
+  Login,
+  Register,
+  Seller }
+  from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <Login /> } />
+        <Route path="/login" element={ <Login /> } />
+        <Route path="/admin" element={ <Admin /> } />
+        <Route path="/checkout" element={ <Checkout /> } />
+        <Route path="/customerdetails" element={ <CustomerDetails /> } />
+        <Route path="/customerorders" element={ <CustomerOrders /> } />
+        <Route path="/register" element={ <Register /> } />
+        <Route path="/seller" element={ <Seller /> } />
       </Routes>
     </BrowserRouter>
   );
