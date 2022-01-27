dc9ca267e8063f1f299f81f54aba3e78d018b1a9
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2022-01-26 21:57:11 -0300

    feat: `data-testid` adicionado ao `NavBar.jsx`

--- front-end/src/components/NavBar/index.jsx
+++ front-end/src/components/NavBar/index.jsx
@@ -6,11 +6,19 @@ export default class NavBar extends Component {
     const { user } = this.props;
     return (
       <div>
-        <div>Produtos</div>
-        <div>Meus Pedidos</div>
+        <div data-testid="customer_products__element-navbar-link-products">
+          Produtos
+        </div>
+        <div data-testid="customer_products__element-navbar-link-orders">
+          Meus Pedidos
+        </div>
 
-        <div>{user}</div>
-        <div>Sair</div>
+        <div data-testid="customer_products__element-navbar-user-full-name">
+          {user}
+        </div>
+        <div data-testid="customer_products__element-navbar-link-logout">
+          Sair
+        </div>
       </div>
     );
   }
