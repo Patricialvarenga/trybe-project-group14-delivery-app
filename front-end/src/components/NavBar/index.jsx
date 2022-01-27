3b16463301cc4bd8740f121283af1cec74900a68
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2022-01-27 17:39:58 -0300

    fix: `NavBar.js` de classe para funcional

--- front-end/src/components/NavBar/index.jsx
+++ front-end/src/components/NavBar/index.jsx
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class NavBar extends Component {
-  render() {
-    const { user } = this.props;
-    return (
-      <div>
-        <div data-testid="customer_products__element-navbar-link-products">
-          Produtos
-        </div>
-        <div data-testid="customer_products__element-navbar-link-orders">
-          Meus Pedidos
-        </div>
-
-        <div data-testid="customer_products__element-navbar-user-full-name">
-          {user}
-        </div>
-        <div data-testid="customer_products__element-navbar-link-logout">
-          Sair
-        </div>
+export default function NavBar({ user }) {
+  return (
+    <div>
+      <div data-testid="customer_products__element-navbar-link-products">
+        Produtos
       </div>
-    );
-  }
+      <div data-testid="customer_products__element-navbar-link-orders">
+        Meus Pedidos
+      </div>
+
+      <div data-testid="customer_products__element-navbar-user-full-name">
+        {user}
+      </div>
+      <div data-testid="customer_products__element-navbar-link-logout">
+        Sair
+      </div>
+    </div>
+  );
 }
 
 NavBar.propTypes = {
