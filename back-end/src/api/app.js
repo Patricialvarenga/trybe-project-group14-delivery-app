6df33c3c556c38979d9fc1a99eb13750d628fef8
Michel Pereira <michelbecauro@gmail.com>
2022-01-28 18:50:25 -0300

    feat: add Cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const root = require('../controllers/root');
 const error = require('../global/middlewares/error');
 
 const app = express();
+
+app.use(function(_req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    app.use(cors());
+    next();
+ });
 
 app.use(bodyParser.json());
 app.use(root);
