d7e5e7b5d87112ad877088a0442defb447044ecf
Elias Forte <82238233+eliforte@users.noreply.github.com>
2022-01-29 14:28:56 -0300

    fix: register return

--- back-end/src/controllers/users/register/register.js
+++ back-end/src/controllers/users/register/register.js
@@ -5,7 +5,7 @@ module.exports = async (req, res, next) => {
   try {
     const { email, password, name, role = 'customer' } = req.body;
     const newRegister = await register({ email, password, name, role });
-    return res.status(CREATED).json({ newRegister });
+    return res.status(CREATED).json(newRegister);
   } catch (err) {
     console.error(err);
     next(err);
