d903c9d54cc5aa778c0eb74f5ab36a3cefd06332
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2022-01-31 16:59:02 -0300

    fix: ordem das seeds

--- back-end/src/database/seeders/20220125204250-users.js
+++ back-end/src/database/seeders/20220125204250-users.js
@@ -3,11 +3,6 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
       await queryInterface.bulkInsert('users', [{
-        name: 'Cliente Zé Birita',
-        email: 'zebirita@email.com',
-        password: '1c37466c159755ce1fa181bd247cb925',
-        role: 'customer'
-      },{
         name: 'Delivery App Admin',
         email: 'adm@deliveryapp.com',
         password: 'a4c86edecc5aee06eff8fdeda69e0d04',
@@ -17,6 +12,12 @@ module.exports = {
         email: 'fulana@deliveryapp.com',
         password: '3c28d2b0881bf46457a853e0b07531c6',
         role: 'seller'
+      },
+      {
+        name: 'Cliente Zé Birita',
+        email: 'zebirita@email.com',
+        password: '1c37466c159755ce1fa181bd247cb925',
+        role: 'customer'
       }], {});
   },
 
