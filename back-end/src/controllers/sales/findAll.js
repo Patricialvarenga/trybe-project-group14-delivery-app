6f84049e5dc67181a457b4ecd69a3bca00296ea2
Michel Pereira <michelbecauro@gmail.com>
2022-02-01 18:54:32 -0300

    feat: Add missing Controller (findAll)

---
+++ back-end/src/controllers/sales/findAll.js
@@ -0,0 +1,15 @@
+const { OK } = require('http-status-codes').StatusCodes;
+const service = require('../../service/sales');
+
+module.exports = async (req, res, next) => {
+  const { userId: id, role } = req.user;
+  try {
+    
+    const sales = await service.findAll({ id, role });
+
+    res.status(OK).json(sales);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
