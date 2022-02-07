ecfa2641cea6577a091fa9def48dbc5264b5715f
Michel Pereira <michelbecauro@gmail.com>
2022-02-07 04:46:04 -0300

    update: Add [Op.or] in 'register()' findOne() ...
    ... in Register service

--- back-end/src/service/users/register.js
+++ back-end/src/service/users/register.js
@@ -1,21 +1,26 @@
+const { Op } = require('sequelize');
 const md5 = require('md5');
 const { ApiError: { NewError } } = require('../../global/error/apiError');
 const { user } = require('../../database/models');
 const auth = require('../../global/middlewares/auth');
-const { EMAIL_EXIST_409 } = require('../../global/error/messages');
+const { USER__EXIST_409 } = require('../../global/error/messages');
 
-const register = async (inforRegister) => {
-  const userExist = await user.findOne({ where: { email: inforRegister.email } });
-  if (userExist) return NewError(EMAIL_EXIST_409);
-  const passwordMd5 = md5(inforRegister.password);
-  const newUser = { ...inforRegister, password: passwordMd5 };
-  const { dataValues } = await user.create(newUser);
-  delete dataValues.password;
-  const authToken = auth.createToken(dataValues);
-  return {
-    ...dataValues,
-    token: authToken,
+const register = async (infoUser) => {
+  const userExist = await user.findOne({
+    where: { [Op.or]: [{ email: infoUser.email }, { name: infoUser.name }] } });
+
+    if (userExist) return NewError(USER__EXIST_409);
+
+    const passwordMd5 = md5(infoUser.password);
+    const newUser = { ...infoUser, password: passwordMd5 };
+    const { dataValues } = await user.create(newUser);
+    delete dataValues.password;
+    const authToken = auth.createToken(dataValues);
+
+    return {
+      ...dataValues,
+      token: authToken,
+    };
   };
-};
 
 module.exports = register;
