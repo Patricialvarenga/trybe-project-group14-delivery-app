6325b815d35961dce5e1eafe0f603e449c94e936
Michel Pereira <michelbecauro@gmail.com>
2022-02-07 04:35:09 -0300

    update: Add [Op.or] in 'registerByAdm' findOne() ...
    ... in admin service

--- back-end/src/service/users/registerByAdm.js
+++ back-end/src/service/users/registerByAdm.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize');
 const md5 = require('md5');
 const { ApiError: { NewError } } = require('../../global/error/apiError');
 const { user } = require('../../database/models');
@@ -5,8 +6,11 @@ const auth = require('../../global/middlewares/auth');
 const { USER__EXIST_409 } = require('../../global/error/messages');
 
 const registerByAdm = async (infoUser) => {
-  const userExist = await user.findOne({ where: { email: infoUser.email } });
+  const userExist = await user.findOne({
+    where: { [Op.or]: [{ email: infoUser.email }, { name: infoUser.name }] } });
+
     if (userExist) return NewError(USER__EXIST_409);
+
     const passwordMd5 = md5(infoUser.password);
     const newUser = { ...infoUser, password: passwordMd5 };
     const { dataValues } = await user.create(newUser);
